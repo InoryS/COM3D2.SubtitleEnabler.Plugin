d41c82140d2728c0dfebfbc95792adadb8685a42
InoryS <101243352+InoryS@users.noreply.github.com>
2024-09-27 01:54:35 +0800

    init

---
+++ COM3D2.SubtitleEnabler.Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace COM3D2.SubtitleEnabler.Plugin
+{
+    [BepInPlugin("com.inorys.subtitleenabler", "COM3D2.SubtitleEnabler.Plugin", "1.0.0.0")]
+    public class SubtitleEnabler : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(SubtitleEnabler), null);
+        }
+
+
+        [HarmonyPatch(typeof(Product), "IsSupportLanguage")]
+        [HarmonyPrefix]
+        private static bool IsSupportLanguage(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(Product), "supportMultiLanguage")]
+        [HarmonyPostfix]
+        private static void supportMultiLanguage(ref bool __result)
+        {
+            __result = true;
+        }
+
+        [HarmonyPatch(typeof(Product), "supportSubtitles")]
+        [HarmonyPostfix]
+        private static void supportSubtitles(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
