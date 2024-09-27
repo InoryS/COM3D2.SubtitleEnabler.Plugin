e5ec892893c619c28cc62bc04bd9403bcfb0d3c1
InoryS <101243352+InoryS@users.noreply.github.com>
2024-09-27 17:29:13 +0800

    1.0.0.1fix

--- COM3D2.SubtitleEnabler.Plugin/COM3D2.SubtitleEnabler.Plugin.cs
+++ COM3D2.SubtitleEnabler.Plugin/COM3D2.SubtitleEnabler.Plugin.cs
@@ -3,16 +3,31 @@ using HarmonyLib;
 
 namespace COM3D2.SubtitleEnabler.Plugin
 {
-    [BepInPlugin("com.inorys.subtitleenabler", "COM3D2.SubtitleEnabler.Plugin", "1.0.0.0")]
+    [BepInPlugin("com.inorys.subtitleenabler", "COM3D2.SubtitleEnabler.Plugin", "1.0.0.1")]
     public class SubtitleEnabler : BaseUnityPlugin
     {
         public void Awake()
         {
-            Harmony.CreateAndPatchAll(typeof(SubtitleEnabler), null);
+            Harmony.CreateAndPatchAll(typeof(SubtitleEnabler));
+        }
+
+        [HarmonyPatch(typeof(Product), nameof(Product.supportMultiLanguage), MethodType.Getter)]
+        [HarmonyPostfix]
+        private static void supportMultiLanguage(ref bool __result)
+        {
+            __result = true;
         }
 
 
-        [HarmonyPatch(typeof(Product), "IsSupportLanguage")]
+        [HarmonyPatch(typeof(Product), nameof(Product.supportSubtitles), MethodType.Getter)]
+        [HarmonyPostfix]
+        private static void supportSubtitles(ref bool __result)
+        {
+            __result = true;
+        }
+
+
+        [HarmonyPatch(typeof(Product), nameof(Product.IsSupportLanguage))]
         [HarmonyPrefix]
         private static bool IsSupportLanguage(ref bool __result)
         {
@@ -20,18 +35,12 @@ namespace COM3D2.SubtitleEnabler.Plugin
             return false;
         }
 
-        [HarmonyPatch(typeof(Product), "supportMultiLanguage")]
-        [HarmonyPostfix]
-        private static void supportMultiLanguage(ref bool __result)
-        {
-            __result = true;
-        }
 
-        [HarmonyPatch(typeof(Product), "supportSubtitles")]
+        [HarmonyPatch(typeof(Product), nameof(Product.subTitleScenarioLanguage), MethodType.Getter)]
         [HarmonyPostfix]
-        private static void supportSubtitles(ref bool __result)
+        private static void subTitleScenarioLanguage(ref Product.Language __result)
         {
-            __result = true;
+            __result = Product.systemLanguage;
         }
     }
 }
